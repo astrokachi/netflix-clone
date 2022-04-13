4b494bf2dd2413da24a518dba4af697d1aa45565
astrokachi <astrokachi@gmail.com>
2022-04-13 13:13:18 +0100

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import axios from "../axios";
 import requests from "../Request";
+import SkeletonBanner from "./SkeletonBanner";
 
 function Banner() {
 	const [movie, setMovie] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -14,39 +16,49 @@ function Banner() {
 					Math.floor(Math.random() * request.data.results.length - 1)
 				]
 			);
+			await setLoading(false);
 			return request;
 		};
 
 		fetchData();
 	}, []);
 
-
 	const truncate = (string, n) => {
 		return string?.length > n ? string.substr(0, n - 1) + "..." : string;
 	};
 
 	return (
-		<header
-			className="banner"
-			style={{
-				backgroundSize: "cover",
-				backgroundPosition: "center center",
-				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-			}}
-		>
-			<div className="banner__contents">
-				<h1 className="banner__title">
-					{movie?.name || movie?.title || movie.original_name}
-				</h1>
-				<div className="banner__buttons">
-					<button className="banner__button">Play</button>
-					<button className="banner__button">My list</button>
-				</div>
-				<h1 className="banner__description">{truncate(movie?.overview, 200)}</h1>
-			</div>
+		<>
+			{loading ? (
+				<SkeletonBanner></SkeletonBanner>
+			) : (
+				<>
+					<header
+						className="banner"
+						style={{
+							backgroundSize: "cover",
+							backgroundPosition: "center center",
+							backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+						}}
+					>
+						<div className="banner__contents">
+							<h1 className="banner__title">
+								{movie?.name || movie?.title || movie.original_name}
+							</h1>
+							<div className="banner__buttons">
+								<button className="banner__button">Play</button>
+								<button className="banner__button">My list</button>
+							</div>
+							<h1 className="banner__description">
+								{truncate(movie?.overview, 200)}
+							</h1>
+						</div>
 
-			<div className="banner--fadeBottom" />
-		</header>
+						<div className="banner--fadeBottom" />
+					</header>
+				</>
+			)}
+		</>
 	);
 }
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,10 +3,12 @@ import "./Row.css";
 import axios from "../axios";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import SkeletonBody from "./SkeletonBody";
 
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 	const [movies, setMovies] = useState([]);
 	const [trailerUrl, setTrailerUrl] = useState("");
+	const [loading, setLoading] = useState(true);
 
 	const baseUrl = "https://image.tmdb.org/t/p/original/";
 
@@ -14,6 +16,7 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 		const fetchData = async () => {
 			const request = await axios.get(fetchUrl);
 			setMovies(request.data.results);
+			await setLoading(false);
 			return request;
 		};
 
@@ -44,31 +47,47 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 	};
 
 	return (
-		<div className="row">
-			<h2 className="row__title">{title}</h2>
+		<div className="">
+			{loading ? (
+				<SkeletonBody />
+			) : (
+				<div className="row">
+					<h2 className="row__title">{title}</h2>
 
-			<div className="row__posters">
-				{movies.map((movie) =>
-					(isLargeRow && movie?.poster_path) ||
-					(!isLargeRow && movie?.backdrop_path) ? (
-						<div key={movie.id} className="row__content">
-							<img
-								onClick={() => handleClick(movie)}
-								className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-								src={`${baseUrl}${
-									isLargeRow ? movie.poster_path : movie.backdrop_path
-								}`}
-								alt={movie?.name}
-								key={movie.id}
-							/>
-							<div className='row__movieTitle'>{movie.name || movie.original_name || movie.title}</div>
-						</div>
-					) : (
-						""
-					)
-				)}
-			</div>
-			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+					<div className="row__posters">
+						{movies.map((movie) =>
+							(isLargeRow && movie?.poster_path) ||
+							(!isLargeRow && movie?.backdrop_path) ? (
+								<div key={movie.id} className="row__content ">
+									<div
+										className={`row__container ${
+											isLargeRow && "row__largeContainer"
+										}`}
+									>
+										<img
+											onClick={() => handleClick(movie)}
+											className={`row__poster ${
+												isLargeRow && "row__posterLarge"
+											}`}
+											src={`${baseUrl}${
+												isLargeRow ? movie.poster_path : movie.backdrop_path
+											}`}
+											alt={movie?.name}
+											key={movie.id}
+										/>
+									</div>
+									<div className="row__movieTitle">
+										{movie.name || movie.original_name || movie.title}
+									</div>
+								</div>
+							) : (
+								""
+							)
+						)}
+					</div>
+					{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+				</div>
+			)}
 		</div>
 	);
 };
